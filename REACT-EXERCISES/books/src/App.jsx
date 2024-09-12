7b543165b2788ae4158b7c9017af3b25b3cb1807
SOVZDEV <9023130@student.zadkine.nl>
2024-09-12 13:13:40 +0200

    PROGRESS.

--- REACT-EXERCISES/books/src/App.jsx
+++ REACT-EXERCISES/books/src/App.jsx
@@ -3,16 +3,21 @@ import './App.css';
 import BookList from "./components/BookList";
 
 function App() {
+  console.log("Bruuuhhh....")
+  
   return (
     <>
       <Header />
       <BookList />
+
+      <h2>Getal</h2>
+      <button>CLick me!</button>
 
     </>
   )
 };
 
 // SFC SHORTCUT
-// COMPONENT = NEW FILE IN THE FOLDER.
+// COMPONENT = NEW FILE IN THE FOLDER. | eigen ontwikkelde html tag.. (this case, header & booklist)
 
 export default App;
--- REACT-EXERCISES/books/src/components/BookList.jsx
+++ REACT-EXERCISES/books/src/components/BookList.jsx
@@ -1,23 +1,25 @@
+import { useState } from "react";
+
 const BookList = () => {
+    const [book, setBook] = useState([{
+        image: "",
+        title: "Interesting Facts for Curious Minds",
+        author: "Jordan Moore"
+    },
+    {
+        image: "",
+        title: "Atomic Habits",
+        author: "James Clear"
+    },
+    {
+        image: "",
+        title: "Fairy Tale",
+        author: "Stephen King"
+    }
+    ]);
+
     return (
         <>
-            <Header />
-            <Book
-                image="Image 1"
-                title="Interesting Facts for Curious Minds"
-                author="Jordan Moore"
-            />
-            <Book
-                image="Image 2"
-                title="Atomic Habits"
-                author="James Clear"
-            />
-            <Book
-                image="Image 3"
-                title="Fairy Tale"
-                author="Stephen King"
-            />
-
         </>
     );
 }
