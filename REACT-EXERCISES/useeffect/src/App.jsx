bb961ee8f249b3f21b75fa922e91b96d498886c3
Jowielynne Hoogenstrijd <9023130@student.zadkine.nl>
2024-09-22 20:02:59 +0200

    USEEFFECT!!

--- REACT-EXERCISES/useeffect/src/App.jsx
+++ REACT-EXERCISES/useeffect/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Clock from './components/Clock'; 
+import WelcomeMessage from './components/WelcomeMessage';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <h1>React Clock</h1>
       <Clock />
+      <WelcomeMessage/>
     </div>
   );
 }
 
 export default App;
+ 
---
+++ REACT-EXERCISES/useeffect/src/components/WelcomeMessage.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const WelcomeMessage = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    let newMessage = '';
+
+    if (hours < 12) {
+      newMessage = 'Goedemorgen';
+    } else if (hours >= 12 && hours < 18) {
+      newMessage = 'Goedemiddag';
+    } else {
+      newMessage = 'Goedenavond';
+    }
+
+    setMessage(newMessage);
+  }, []);
+  return (
+    <div>
+      <h2>{message}, Welcome!</h2>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
