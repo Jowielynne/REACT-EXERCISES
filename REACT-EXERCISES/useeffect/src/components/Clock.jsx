0436c98fbaec697c29b7531e8addd0933bec8751
Jowielynne Hoogenstrijd <9023130@student.zadkine.nl>
2024-09-22 19:23:50 +0200

    useeffect part 1

---
+++ REACT-EXERCISES/useeffect/src/components/Clock.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+function Clock() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []); 
+
+  return (
+    <div>
+      <h1>Current Time:</h1>
+      <p>{currentTime.toLocaleTimeString()}</p>
+    </div>
+  );
+}
+
+export default Clock;
